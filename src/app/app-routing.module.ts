2dc01180872eb8f1d553571724b9e8304b8bfd41
Smit Mehta <mehtasmit735@gmail.com>
2020-09-11 11:21:31 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AdminguardGuard } from './adminguard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/dashboard', component: DashboardComponent, canActivate: [AdminguardGuard] },
+  { path: 'admin/login', component: LoginComponent },
+  {
+    path: '',
+    redirectTo: 'admin/login',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatInputModule } from '@angular/material/input';
+import { AuthguardserviceService } from "./authguardservice.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthguardserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
